06ce58dc0f4dbc0837a46e69d86326caab08d7bc
chenjian <chenjian@chjgo.com>
2022-03-12 23:31:45 +0800

    feat:修改中间件

--- pkg/route/router.go
+++ pkg/route/router.go
@@ -7,16 +7,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var route *mux.Router
+var router *mux.Router
 
 // SetRoute 设置路由实例，以供 Name2URL 等函数使用
 func SetRoute(r *mux.Router) {
-	route = r
+	router = r
 }
 
 // RouteName2URL 通过路由名称来获取 URL
 func Name2URL(routeName string, pairs ...string) string {
-	url, err := route.Get(routeName).URL(pairs...)
+	url, err := router.Get(routeName).URL(pairs...)
 	if err != nil {
 		logger.LogError(err)
 		return ""
